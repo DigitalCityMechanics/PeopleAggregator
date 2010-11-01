d76fd07c59c0bc98d8332b1ae0613c1c1597fd7a
Jonathan Knapp <jon@coffeeandcode.com>
2010-11-01 18:07:25 -0400

    Added a page for viewing the current poll in the middle column. Changed poll submission to redirect to it for CC project.

---
+++ paproject/web/save_vote.php
@@ -0,0 +1,38 @@
+<?php
+/** !
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* [filename] is a part of PeopleAggregator.
+* [description including history]
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* @author [creator, or "Original Author"]
+* @license http://bit.ly/aVWqRV PayAsYouGo License
+* @copyright Copyright (c) 2010 Broadband Mechanics
+* @package PeopleAggregator
+*/
+?>
+<?php 
+$login_required = FALSE;
+$use_theme = 'Beta';
+include_once "web/includes/page.php";
+require_once "api/Poll/Poll.php";
+if (!empty($_POST['vote'])) {
+  $vote = $_POST['vote'];
+  $vote = html_entity_decode(stripslashes($vote));
+  $poll_id = $_POST['poll_id'];
+  if (PA::$login_uid) {
+    $uid = PA::$login_uid; 
+    $obj = new Poll();
+    $obj->poll_id = $poll_id;
+    $obj->vote = $vote;
+    $obj->user_id = $uid;
+    $obj->is_active = ACTIVE;
+    $obj->save_vote();
+    $location = PA::$url.'/view_poll.php';
+    header("location: $location");
+    exit;
+  }
+}
+ $location = PA::$url.'/view_poll.php';
+ header("location: $location");
+ exit;
+?>
