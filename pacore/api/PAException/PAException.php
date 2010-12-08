250cbaaa5ba28d78dff03fe26dfe4a378f5e3c91
Parag Jagdale <paragjagdale@gmail.com>
2010-12-08 11:42:37 -0500

    Modified PAException to send PA exceptions to hoptoad

--- pacore/api/PAException/PAException.php
+++ pacore/api/PAException/PAException.php
@@ -12,6 +12,7 @@
 ?>
 <?php
 require_once 'PAExceptionCodes.inc';
+require_once "ext/php-hoptoad-notifier/Services/Hoptoad.php";
 /**
 * Class PAException extends the base exception class for the PeopleAggregator
 *
@@ -25,7 +26,7 @@ class PAException extends Exception {
     parent::__construct($exceptionMessage, (int)$exceptionCode);
     $this->code = $exceptionCode;
     $this->message = $exceptionMessage;
-
+    
     try{
 	    // Gets the matching HTTP Status code for the exception code
 	    list($code_string, $httpStatusCode) = pa_get_error_name($this->code);
@@ -35,11 +36,20 @@ class PAException extends Exception {
 
 			// log the occrance in php error log
 			//user_error(print_r($httpStatusCode, true), E_USER_WARNING);
-	    }	    
+	    }
     }catch(Exception $ex){
     	// ignore this exception. No need to throw it since the HttpStatusCode is
     	// not important enough to throw over the real exception    	
     }
+    
+    try{
+    	/*$hoptoad = new Services_Hoptoad("b16b886469e9c1f3dccfdbb11e56123f", 'staging', 'curl');
+    	$hoptoad->exceptionHandler($this);*/
+    }
+    catch(Exception $e){
+		// ignore exception since posting to Hoptoad is not fatal    	
+    }
+    
     
   }
 }
